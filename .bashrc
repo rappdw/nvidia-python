63edd41f6ca9f3cfa653cb073b8f6fedefac3537
Dan Rapp <drapp@proofpoint.com>
2018-03-24 07:55:47 -0600

    Implement docker best practices:  - Fewer layers  - Use of ENTRYPOINT & CMD

--- .bashrc
+++
@@ -1,1 +0,0 @@
-if [ -f /etc/profile.d/.cpu-gpu-env.sh ]; then . /etc/profile.d/.cpu-gpu-env.sh; fi
---
+++ docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+WORKDIR=${WORKDIR:-"/workdir"}
+
+# install the module mounted in $WORKDIR
+if [ -e $WORKDIR/setup.py ]; then
+    pip install -e $WORKDIR
+fi
+
+# source the correct CPU or GPU virtual environment
+CPU_GPU_ENV=${CPU_GPU_ENV:-"/cpu-env"}
+if [ -e $CPU_GPU_ENV ]; then
+    . $CPU_GPU_ENV
+fi
+
+# fix any python script shebangs in the /usr/local/bin directory
+fix-shebang
+
+exec "$@"
