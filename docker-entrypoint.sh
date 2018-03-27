3717aacecd9a54d15506249acfea7a5468cd8806
Dan Rapp <drapp@proofpoint.com>
2018-03-27 12:31:31 -0600

    Broaden support of cpu/gpu env determiniation

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,9 +7,13 @@ if [ -e $WORKDIR/setup.py ]; then
     pip install -e $WORKDIR
 fi
 
-# source the correct CPU or GPU virtual environment using NVIDIA_VISIBLE_DEVICES
+# source the correct CPU or GPU virtual environment using NVIDIA_VISIBLE_DEVICES and CPU_GPU_ENV
 # Values are documented here: https://github.com/nvidia/nvidia-container-runtime#nvidia_visible_devices
-if [ -z $NVIDIA_VISIBLE_DEVICES ] || [ "$NVIDIA_VISIBLE_DEVICES" = "void" ]; then
+if [ -z "$NVIDIA_VISIBLE_DEVICES" ] || \
+   [ "$NVIDIA_VISIBLE_DEVICES" = "void" ] || \
+   [ -z "$CPU_GPU_ENV" ] || \
+   [ "$CPU_GPU_ENV" == "/cpu-env" ]
+then
     . /.cpu-env/bin/activate
 else
     . /.gpu-env/bin/activate
