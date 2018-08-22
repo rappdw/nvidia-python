f76d6dde79deee3f25a1b64a0bd40ab54b911f2d
Daniel Rapp <rappdw@gmail.com>
2018-08-22 06:26:52 -0600

    Simplify: remove support for CPU/GPU in same image (#2)
    Upgrade nvidia support to CUDA 9.2/cudnn7

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,25 +1,12 @@
 #!/usr/bin/env bash
 
+source /.venv/bin/activate
+
 WORKDIR=${WORKDIR:-"/workdir"}
 
 # install the module mounted in $WORKDIR
 if [ -e $WORKDIR/setup.py ]; then
     pip install -e $WORKDIR
 fi
-
-# source the correct CPU or GPU virtual environment using NVIDIA_VISIBLE_DEVICES and CPU_GPU_ENV
-# Values are documented here: https://github.com/nvidia/nvidia-container-runtime#nvidia_visible_devices
-if [ -z "$NVIDIA_VISIBLE_DEVICES" ] || \
-   [ "$NVIDIA_VISIBLE_DEVICES" = "void" ] || \
-   [ -z "$CPU_GPU_ENV" ] || \
-   [ "$CPU_GPU_ENV" == "/cpu-env" ]
-then
-    . /.cpu-env/bin/activate
-else
-    . /.gpu-env/bin/activate
-fi
-
-# fix any python script shebangs in the /usr/local/bin directory
-fix-shebang
 
 exec "$@"
